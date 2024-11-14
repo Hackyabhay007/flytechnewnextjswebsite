8b8d26199b060e08d189b11e8ec64f87a2955544
Mukesh <lakkakulababblu@gmail.com>
2024-11-14 10:20:12 +0530

    added firebase keys to env file

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDy-ZjI9dhzudUugya2nZCCMRUlqIgUfvs",
-  authDomain: "fyt-flyyourtech.firebaseapp.com",
-  projectId: "fyt-flyyourtech",
-  storageBucket: "fyt-flyyourtech.firebasestorage.app",
-  messagingSenderId: "1079237041732",
-  appId: "1:1079237041732:web:8609c287f234b0b5a9a611",
-  measurementId: "G-C9MFCG8P4G"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
